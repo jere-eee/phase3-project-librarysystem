7e02cfc78914494e9323977becf924e6fb6fe6d9
Akanle Jeremy <akanlejeremy@gmail.com>
2024-12-16 14:53:51 +0300

    Adding more functionality to Book model

--- models/book.py
+++ models/book.py
@@ -25,17 +25,58 @@ class Book:
             cursor.execute("INSERT INTO books (title, author, copies) VALUES (?, ?, ?)", (self._title, self._author, self._copies))
             conn.commit()
             self._id = cursor.lastrowid
+            print(f"'{self._title}' successfully added to the database!")
         except sqlite3.IntegrityError as e:
             print(f"Error creating book: {e}")
-        
-    def delete(self):
+    
+    @classmethod
+    def delete(title):
         """Delete current book from database."""
+        new_title = title_formatter(title)
         try:
-            cursor.execute("DELETE FROM books WHERE id = ?", (self._id))
+            cursor.execute("DELETE FROM books WHERE title = ?", (new_title,))
             conn.commit()
-            print(f"Book with id {self._id} deleted.")
+            print(f"The book '{new_title}' has been successfully deleted.")
         except Exception as e:
             print(f"Error deleting book: {e}")
             
+    @classmethod
+    def get_all(cls):
+        try:
+            cursor.execute("SELECT title, author, copies FROM books")
+            books = cursor.fetchall()
+            return books if books else []
+        except sqlite3.IntegrityError as e:
+            print(f"Error getting books: {e}") 
     
+    @classmethod
+    def find_by_id(cls, book_id):
+        try:
+            cursor.execute("SELECT id, title, author, copies FROM books WHERE id = ?", (book_id,))
+            return cursor.fetchone()
+        except sqlite3.IntegrityError as e:
+            print(f"Error finding book: {e}")
+    
+    @classmethod
+    def borrow(cls, book_id):
+        try:
+            cursor.execute("SELECT copies FROM books WHERE id = ?", (book_id))
+            copies = cursor.fetchone()[0]
+            if copies > 0:
+                   cursor.execute("UPDATE books SET copies = copies - 1 WHERE id = ?", (book_id,))
+                   conn.commit()
+                   print(f"Book of id {book_id} borrowed.")
+            else:
+                print(f"Book of id {book_id} is not available for borrowing.")
+        except sqlite3.IntegrityError as e:
+            print(f"Error borrowing book, check id?: {e}")
+            
+    @classmethod
+    def return_book(cls, book_id):
+        cursor.execute("UPDATE books SET copies = copies + 1 WHERE id = ?", (book_id,))
+        conn.commit()
+        print(f"Book with id {book_id} returned.")
+        
+    def __repr__(self):
+        return f"{self._title} by {self._author}, (Copies:{self._copiess})"
     
